82ade97b1f028559f8f29ce26340ff1634876586
yinyanghns <63975831+yinyanghns@users.noreply.github.com>
2023-03-19 01:03:43 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,141 @@
+$(function () {
+
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+function mostrarPrecios1() {
+        var precios = document.getElementById("precios1");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios2() {
+        var precios = document.getElementById("precios2");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+
+ function mostrarPrecios3() {
+        var precios = document.getElementById("precios3");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+  function mostrarPrecios4() {
+        var precios = document.getElementById("precios4");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+     
+function mostrarPrecios5() {
+        var precios = document.getElementById("precios5");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+     
+function mostrarPrecios6() {
+        var precios = document.getElementById("precios6");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios7() {
+        var precios = document.getElementById("precios7");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+      
+function mostrarPrecios8() {
+        var precios = document.getElementById("precios8");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+     
+function mostrarPrecios9() {
+        var precios = document.getElementById("precios9");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios10() {
+        var precios = document.getElementById("precios10");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios11() {
+        var precios = document.getElementById("precios11");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios12() {
+        var precios = document.getElementById("precios12");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+     
+function mostrarPrecios13() {
+        var precios = document.getElementById("precios13");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+
+function mostrarPrecios14() {
+        var precios = document.getElementById("precios14");
+        if (precios.style.display === "none") {
+          precios.style.display = "block";
+        } else {
+          precios.style.display = "none";
+        }
+      }
+                                   
